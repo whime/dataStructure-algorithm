968328a8fba36b68aef46e5ca399e02d01508329
whime <whime@foxmail.com>
2021-08-17 07:32:16 +0800

    2021.08.17_dailyAttendance

---
+++ Leetcode/simple/551. 学生出勤记录 I.py
@@ -0,0 +1,35 @@
+"""
+给你一个字符串 s 表示一个学生的出勤记录，其中的每个字符用来标记当天的出勤情况（缺勤、迟到、到场）。记录中只含下面三种字符：
+
+'A'：Absent，缺勤
+'L'：Late，迟到
+'P'：Present，到场
+如果学生能够 同时 满足下面两个条件，则可以获得出勤奖励：
+
+按 总出勤 计，学生缺勤（'A'）严格 少于两天。
+学生 不会 存在 连续 3 天或 3 天以上的迟到（'L'）记录。
+如果学生可以获得出勤奖励，返回 true ；否则，返回 false 。
+"""
+
+class Solution(object):
+    def checkRecord(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        Lcount=0
+        Acount=0
+        for state in s:
+            if state=="A":
+                if Acount!=0:
+                    return False
+                else:
+                    Acount+=1
+
+            if state=="L":
+                Lcount+=1
+                if Lcount==3:
+                    return False
+            else:
+                Lcount=0
+        return True
